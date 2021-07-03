f19c6ffa6b772f228a8903c4a04cdea376736749
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-07-03 09:32:47 -0500

    init
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import initialData from './initial-data'; 
+import React, {useState} from 'react'; 
+import Column from './Column'; 
+import {DragDropContext} from 'react-beautiful-dnd'; 
 function App() {
+  const [initData, setInitData] = useState(initialData); 
+  const onDragEnd = result => {
+    const {destination, source, draggableId} = result;
+    if(!destination){
+      return; 
+    };
+    if(
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ){
+      return;
+    }; 
+    const column = initData.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds); 
+    newTaskIds.splice(source.index, 1); 
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+    const newState = {
+      ...initData,
+      columns: {
+        ...initData.columns,
+        [newColumn.id]: newColumn
+      }
+    }; 
+    setInitData(newState);
+    console.log(initData.columns['column-1'].taskIds);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd} >
+      {initData.columnOrder.map(columnId => {
+        const column = initData.columns[columnId];
+        const tasks = column.taskIds.map(taskId => initData.tasks[taskId]); 
+        return <Column key={column.id} column={column} tasks={tasks} />; 
+      })}
+    </DragDropContext>
   );
 }
 
---
+++ src/Task.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components'; 
+import {Draggable} from 'react-beautiful-dnd'; 
+const Container = styled.div`
+  border: 1px solid lightgray;
+  padding:8px; 
+  margin-bottom: 8px;
+  border-radius:2px;
+  background-color: white;
+`; 
+const Task = props => {
+  return(
+    <Draggable draggableId={props.task.id} index={props.index} >
+      {(provided)=> (
+        <Container
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {props.task.content}
+        </Container>
+      )}
+    </Draggable>
+  ); 
+}; 
+export default Task;
+
